76ba056c2b4b483ad1275f6f914d626f42ebf475
17Shanto <shahporanshanto23@gmail.com>
2025-09-04 20:34:27 +0600

    Connect to mongodb

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,11 +1,16 @@
 import { Server } from "http";
 import { app } from "./app";
+import mongoose from "mongoose";
 
 let server: Server;
 const PORT = 500;
 
 async function main() {
   try {
+    await mongoose.connect(
+      "mongodb+srv://shahporanshanto23:gDuSbBDKNuF6T9Ig@note-app-cluster.aff1yy7.mongodb.net/library-management-api?retryWrites=true&w=majority&appName=note-app-cluster"
+    );
+    console.log("Database mongodb has been connected");
     server = app.listen(PORT, () => {
       console.log(`App is listening on port ${PORT}`);
     });
