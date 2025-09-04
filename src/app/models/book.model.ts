fcf0f8810c5fe0e5e159ebac75412639eda50473
17Shanto <shahporanshanto23@gmail.com>
2025-09-04 22:12:03 +0600

    Fix book schema and model

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -1,24 +1,38 @@
 import mongoose, { Schema } from "mongoose";
 import { IBook } from "../interfaces/book.interface";
 
-const bookSchema = new Schema<IBook>({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: {
-    type: String,
-    enum: [
-      "FICTION",
-      "NON_FICTION",
-      "SCIENCE",
-      "HISTORY",
-      "BIOGRAPHY",
-      "FANTASY",
-    ],
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: { type: String, required: false },
+    copies: {
+      type: Number,
+      required: true,
+      min: 0,
+      validate: {
+        validator: Number.isInteger,
+        message: "{VALUE} is not an integer value",
+      },
+    },
+    available: { type: Boolean, default: true },
   },
-  isbn: { type: String, required: true, unique: true },
-  description: { type: String, required: false },
-  copies: { type: Number, required: true },
-  available: { type: Boolean, default: true },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-export const Book = mongoose.model("Book", bookSchema);
+export const Book = mongoose.model<IBook>("Book", bookSchema);
