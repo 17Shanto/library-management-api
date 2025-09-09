ed72645a584a4e26f8c4a9e185570df7a870b2f4
17Shanto <shahporanshanto23@gmail.com>
2025-09-09 10:04:25 +0600

    add dotenv

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,15 +1,16 @@
 import { Server } from "http";
 import { app } from "./app";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 let server: Server;
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
+const MONGODB_URL = process.env.MONGODB_URL as string;
 
 async function main() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://shahporanshanto23:gDuSbBDKNuF6T9Ig@note-app-cluster.aff1yy7.mongodb.net/library-management-api?retryWrites=true&w=majority&appName=note-app-cluster"
-    );
+    await mongoose.connect(`${MONGODB_URL}`);
     console.log("Database mongodb has been connected");
     server = app.listen(PORT, () => {
       console.log(`App is listening on port ${PORT}`);
